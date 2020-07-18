9fcec0ae95082143106d08469b3517b21c5aad22
nihitb06 <nihitb06.dev@gmail.com>
2020-07-18 22:16:39 +0530

    Added init command functionality

--- libpvc.py
+++ libpvc.py
@@ -38,6 +38,161 @@ argsubparsers = argparser.add_subparsers(
 )
 argsubparsers.required = True
 
+# Add init command argument
+argsp = argsubparsers.add_parser(
+    "init", help="Initialize a new, empty repository."
+)
+argsp.add_argument("path",
+                   metavar="directory",
+                   nargs="?",
+                   default=".",
+                   help="Where to create the repository.")
+
+# }}}
+
+# Git Objects {{{
+
+class GitRepository(object):
+    worktree = None
+    gitdir = None
+    conf = None
+
+    def __init__(self, path, force=False):
+        self.worktree = path
+        self.gitdir = os.path.join(path, ".git")
+
+        if not (force or os.path.isdir(self.gitdir)):
+            raise Exception("Not a git repository %s"%path)
+
+        # Read configuration file in .git/config
+        self.conf = configparser.ConfigParser()
+        cf = repo_file(self, "config")
+
+        if cf and os.path.exists(cf):
+            self.conf.read([cf])
+        elif not force:
+            raise Exception("Configuration file missing")
+
+        if not force:
+            vers = int(self.conf.get(
+                "core", "repositoryformatversion"
+            ))
+            if vers != 0:
+                raise Exception(
+                    "Unsupported repositoryformatversion %s"
+                    % vers
+                )
+
+# }}}
+
+# Helper Functions {{{
+
+# For file Paths {{
+
+def repo_path(repo, *path):
+    # Compute the path under repo's gitdir
+    return os.path.join(repo.gitdir, *path)
+
+def repo_file(repo, *path, mkdir=False):
+    # Same as repo path but can create dirname(*path) if
+    # absent
+    if repo_dir(repo, *path, mkdir=mkdir):
+        return repo_path(repo, *path)
+
+def repo_dir(repo, *path, mkdir=False):
+    # Same as repo_path, but mkdir *path if absent and if
+    # mkdir is true
+    path = repo_path(repo, *path)
+
+    if os.path.exists(path):
+        if os.path.isdir(path):
+            return path
+        else:
+            raise Exception("Not a directory %s" % path)
+
+    if mkdir:
+        os.makedirs(path)
+        return path
+    else:
+        return None
+
+def repo_find(path=".", required=True):
+    path = os.path.realpath(path)
+
+    if os.path.isdir(os.path.join(path, ".git")):
+        return GitRepository(path)
+
+    # If we haven't returned, recurse in parent, if w
+    parent = os.path.realpath(os.path.join(path, ".."))
+
+    if parent == path:
+        # Bottom case
+        # os.path.join("/", "..") == "/":
+        # If parent==path, then path is root.
+        if required:
+            raise Exception("No git directory.")
+        else:
+            return None
+
+    # Recursive case
+    return repo_find(parent, required)
+
+# }}
+
+# Repository Creation {{
+
+def repo_create(path):
+    repo = GitRepository(path, True)
+
+    if os.path.exists(repo.worktree):
+        if not os.path.isdir(repo.worktree):
+            raise Exception("%s is not a directory!" % path)
+        if not os.listdir(repo.worktree):
+            raise Exception("%s is not empty!" % path)
+    else:
+        os.makedirs(repo.worktree)
+
+    assert(repo_dir(repo, "branches", mkdir=True))
+    assert(repo_dir(repo, "objects", mkdir=True))
+    assert(repo_dir(repo, "refs", "tags", mkdir=True))
+    assert(repo_dir(repo, "refs", "heads", mkdir=True))
+
+    # .git/description
+    with open(repo_file(repo, "description"), "w") as f:
+        f.write(
+            "Unnamed repository; edit this file to name \
+            repository\n"
+        )
+
+    # .git/HEAD
+    with open(repo_file(repo, "HEAD"), "w") as f:
+        f.write("ref: refs/heads/master\n")
+
+    with open(repo_file(repo, "config"), "w") as f:
+        config = repo_default_config()
+        config.write(f)
+
+    return repo
+
+def repo_default_config():
+    ret = configparser.ConfigParser()
+
+    ret.add_section("core")
+    ret.set("core", "repositoryformatversion", "0")
+    ret.set("core", "filemode", "false")
+    ret.set("core", "bare", "false")
+
+    return ret
+
+# }}
+
+# }}}
+
+# Bridge functions (used in main()) {{{
+
+def cmd_init(args):
+    repo_create(args.path)
+
 # }}}
 
 # Main Function {{{
