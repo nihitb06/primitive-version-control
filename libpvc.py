72a94dcdff7e292170434a44ef2534ce2d7f90d8
nihitb06 <nihitb06.dev@gmail.com>
2020-07-18 15:48:17 +0530

    Added a main function to library file

--- libpvc.py
+++ libpvc.py
@@ -26,3 +26,40 @@ import sys
 import zlib
 
 # }}}
+
+# Initialization of Argument Parser {{{
+
+argparser = argparse.ArgumentParser(
+    description="The Comment Tracker"
+)
+# Add subparsers to parse commands
+argsubparsers = argparser.add_subparsers(
+    title="Commands", dest="command"
+)
+argsubparsers.required = True
+
+# }}}
+
+# Main Function {{{
+
+def main(argv=sys.argv[1:]):
+    # Get arguments
+    args = argparser.parse_args(argv)
+
+    # Call appropriate function based on command
+    if   args.command == "add"        : cmd_add(args)
+    elif args.command == "cat-file"   : cmd_cat_file(args)
+    elif args.command == "checkout"   : cmd_checkout(args)
+    elif args.command == "commit"     : cmd_commit(args)
+    elif args.command == "hash-object": cmd_hash_obj(args)
+    elif args.command == "init"       : cmd_init(args)
+    elif args.command == "log"        : cmd_log(args)
+    elif args.command == "ls-tree"    : cmd_ls_tree(args)
+    elif args.command == "merge"      : cmd_merge(args)
+    elif args.command == "rebase"     : cmd_rebase(args)
+    elif args.command == "rev-parse"  : cmd_rev_parse(args)
+    elif args.command == "rm"         : cmd_rm(args)
+    elif args.command == "show-ref"   : cmd_show_ref(args)
+    elif args.command == "tag"        : cmd_tag(args)
+
+# }}}
