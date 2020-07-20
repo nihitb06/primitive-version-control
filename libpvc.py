3eabf679228134f8b1ac6c4dfc234af08745e4cd
nihitb06 <nihitb06.dev@gmail.com>
2020-07-20 12:58:15 +0530

    Added cat-file command functionality

--- libpvc.py
+++ libpvc.py
@@ -38,7 +38,8 @@ argsubparsers = argparser.add_subparsers(
 )
 argsubparsers.required = True
 
-# Add init command argument
+# Add init command argument {{
+
 argsp = argsubparsers.add_parser(
     "init", help="Initialize a new, empty repository."
 )
@@ -47,6 +48,24 @@ argsp.add_argument("path",
                    nargs="?",
                    default=".",
                    help="Where to create the repository.")
+
+# }}
+
+# Add cat-file command argument {{
+
+argsp = argsubparsers.add_parser("cat-file",
+    help="Provide content of repository objects")
+
+argsp.add_argument("type",
+    metavar="type",
+    choices=["blob", "commit", "tag", "tree"],
+    help="Specify the type")
+
+argsp.add_argument("object",
+    metavar="object",
+    help="The object to display")
+
+# }}
 
 # }}}
 
@@ -289,8 +308,24 @@ def object_write(obj, actually_write=True):
 
 # Bridge functions (used in main()) {{{
 
+# Init {{
+
 def cmd_init(args):
     repo_create(args.path)
+
+# }}
+
+# Cat-File {{
+
+def cmd_cat_file(args):
+    repo = repo_find()
+    cat_file(repo, args.object, fmt=args.type.encode())
+
+def cat_file(repo, obj, fmt=None):
+    obj = object_read(repo, object_find(repo, obj, fmt=fmt))
+    sys.stdout.buffer.write(obj.serialize())
+
+# }}
 
 # }}}
 
