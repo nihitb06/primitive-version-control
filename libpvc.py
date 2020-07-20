f79666d14dad8999aaf3e3b910a926aa5bd84dec
nihitb06 <nihitb06.dev@gmail.com>
2020-07-20 12:52:57 +0530

    Added object write functionality

--- libpvc.py
+++ libpvc.py
@@ -255,6 +255,25 @@ def object_find(repo, name, fmt=None, follow=True):
     # TODO: Implement function
     return name
 
+def object_write(obj, actually_write=True):
+    data = obj.serialize()
+
+    # Add header
+    result = obj.fmt + b' ' + str(len(data)).encode()
+        + b'\x00' + data
+    
+    # Compute Hash
+    sha = hashlib.sha1(result).hexdigest()
+
+    if actually_write:
+        path = repo_file(obj.repo, "objects", sha[0:2],
+            sha[2:], mkdir=actually_write)
+
+        with open(path, "wb") as f:
+            f.write(zlib.compress(result))
+
+    return sha
+
 # }}
 
 # }}}
