7d1ec1e7d050a60cc9b2b7974cec7e1b342ad00a
nihitb06 <nihitb06.dev@gmail.com>
2020-07-20 15:01:02 +0530

    Added Tree parsing and serialization

--- libpvc.py
+++ libpvc.py
@@ -181,6 +181,24 @@ class GitCommit(GitObject):
     def serialize(self):
         return kvlm_serialize(self.kvlm)
 
+# Object for Tree Leaf
+# Represents a Tree object in Git
+class GitTreeLeaf(object):
+    def __init__(self, mode, path, sha):
+        self.mode = mode
+        self.path = path
+        self.sha = sha
+
+# GitObject for type Tree
+class GitTree(GitObject):
+    fmt=b'tree'
+
+    def deserialize(self, data):
+        self.items = tree_parse(data)
+
+    def serialize(self):
+        return tree_serialize(self)
+
 # }}}
 
 # Helper Functions {{{
@@ -438,6 +456,54 @@ def kvlm_serialize(kvlm):
     # Append message
     ret += b'\n' + kvlm[b'']
 
+    return ret
+
+# }}
+
+# Tree Parsing {{
+
+# Parsing a single Tree Node
+def tree_parse_one(raw, start=0):
+    # Find the space terminator of the mode
+    x = raw.find(b' ', start)
+    assert(x-start == 5 or x-start==6)
+
+    # Read the mode
+    mode = raw[start:x]
+
+    # Find the NULL terminator of the path
+    y = raw.find(b'\x00', x)
+    # and read the path
+    path = raw[x+1:y]
+
+    # Read the SHA and convert to an hex string
+    sha = hex(
+        int.from_bytes(raw[y+1:y+21], "big"))[2:] 
+        # hex() adds 0x in front, we don't want that.
+
+    return y+21, GitTreeLeaf(mode, path, sha)
+
+# Calls the previous parser in a loop for chained trees
+def tree_parse(raw):
+    pos = 0
+    max = len(raw)
+    ret = list()
+    while pos < max:
+        pos, data = tree_parse_one(raw, pos)
+        ret.append(data)
+
+    return ret
+
+# Tree Serializer
+def tree_serialize(obj):
+    ret = b''
+    for i in obj.items:
+        ret += i.mode
+        ret += b' '
+        ret += i.path
+        ret += b'\x00'
+        sha = int(i.sha, 16)
+        ret += sha.to_bytes(20, byteorder="big")
     return ret
 
 # }}
