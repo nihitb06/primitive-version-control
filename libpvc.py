1e979433d70a91d45a8084e948994794f2417472
nihitb06 <nihitb06.dev@gmail.com>
2020-07-20 15:57:48 +0530

    Added show-ref command functionality

--- libpvc.py
+++ libpvc.py
@@ -162,6 +162,45 @@ argsp.add_argument("path",
 argsp = argsubparsers.add_parser("show-ref", 
     help="List references.")
 
+# Add tag command argument {{
+
+argsp = argsubparsers.add_parser(
+    "tag",
+    help="List and create tags")
+
+argsp.add_argument("-a",
+    action="store_true",
+    dest="create_tag_object",
+    help="Whether to create a tag object")
+
+argsp.add_argument("name",
+    nargs="?",
+    help="The new tag's name")
+
+argsp.add_argument("object",
+    default="HEAD",
+    nargs="?",
+    help="The object the new tag will point to")
+
+# }}
+
+# Add rev-parse command argument {{
+
+argsp = argsubparsers.add_parser("rev-parse",
+    help="Parse revision (or other objects )identifiers")
+
+argsp.add_argument("--wyag-type",
+    metavar="type",
+    dest="type",
+    choices=["blob", "commit", "tag", "tree"],
+    default=None,
+    help="Specify the expected type")
+
+argsp.add_argument("name",
+    help="The name to parse")
+
+# }}
+
 # }}}
 
 # Git Objects {{{
@@ -258,6 +297,11 @@ class GitTree(GitObject):
 
     def serialize(self):
         return tree_serialize(self)
+
+# GitObject for type Tag
+# Almost same as GitCommit
+class GitTag(GitCommit):
+    fmt = b'tag'
 
 # }}}
 
@@ -404,8 +448,83 @@ def object_read(repo, sha):
         return c(repo, raw[y+1:])
 
 def object_find(repo, name, fmt=None, follow=True):
-    # TODO: Implement function
-    return name
+    sha = object_resolve(repo, name)
+
+    if not sha:
+        raise Exception(
+            "No such reference {0}.".format(name)
+        )
+
+    if len(sha) > 1:
+        raise Exception(
+            "Ambiguous reference {0}: Candidates are:\n - {1}.".format(name,  "\n - ".join(sha))
+        )
+
+    sha = sha[0]
+
+    if not fmt:
+        return sha
+
+    while True:
+        obj = object_read(repo, sha)
+
+        if obj.fmt == fmt:
+            return sha
+
+        if not follow:
+            return None
+
+        # Follow tags
+        if obj.fmt == b'tag':
+            sha = obj.kvlm[b'object'].decode("ascii")
+        elif obj.fmt == b'commit' and fmt == b'tree':
+            sha = obj.kvlm[b'tree'].decode("ascii")
+        else:
+            return None
+
+def object_resolve(repo, name):
+    '''Resolve name to an object hash in repo.
+
+    This function is aware of:
+
+    - the HEAD literal
+    - short and long hashes
+    - tags
+    - branches
+    - remote branches'''
+    candidates = list()
+    hashRE = re.compile(r"^[0-9A-Fa-f]{1,16}$")
+    smallHashRE = re.compile(r"^[0-9A-Fa-f]{1,16}$")
+
+    # Empty string?  Abort.
+    if not name.strip():
+        return None
+
+    # Head is nonambiguous
+    if name == "HEAD":
+        return [ ref_resolve(repo, "HEAD") ]
+
+
+    if hashRE.match(name):
+        if len(name) == 40:
+            # This is a complete hash
+            return [ name.lower() ]
+        elif len(name) >= 4:
+            # This is a small hash 4 seems to be the 
+            # minimal length for git to consider something 
+            # a short hash.
+            # This limit is documented in man git-rev-parse
+            name = name.lower()
+            prefix = name[0:2]
+            path = repo_dir(repo, "objects", 
+                prefix, mkdir=False)
+            if path:
+                rem = name[2:]
+                for f in os.listdir(path):
+                    if f.startswith(rem):
+                        candidates.append(prefix + f)
+
+    return candidates
 
 def object_write(obj, actually_write=True):
     data = obj.serialize()
@@ -596,6 +715,21 @@ def ref_list(repo, path=None):
 
 # }}
 
+# Tag {{
+
+def cmd_tag(args):
+    repo = repo_find()
+
+    if args.name:
+        tag_create(args.name,
+            args.object,
+            type="object" if args.create_tag_object else "ref")
+    else:
+        refs = ref_list(repo)
+        show_ref(repo, refs["tags"], with_hash=False)
+
+# }}
+
 # }}}
 
 # Bridge functions (used in main()) {{{
@@ -723,6 +857,20 @@ def show_ref(repo, refs, with_hash=True, prefix=""):
                 prefix="{0}{1}{2}".format(
                     prefix, "/" if prefix else "", k)
             )
+
+# }}
+
+# Rev-Parse {{
+
+def cmd_rev_parse(args):
+    if args.type:
+        fmt = args.type.encode()
+
+    repo = repo_find()
+
+    print (object_find(
+        repo, args.name, args.type, follow=True
+    ))
 
 # }}
 
