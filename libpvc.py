a4507fb583372b9fcdd0ec18e46809724f8db132
nihitb06 <nihitb06.dev@gmail.com>
2020-07-20 13:06:47 +0530

    Added hash-object command functionality

--- libpvc.py
+++ libpvc.py
@@ -67,6 +67,30 @@ argsp.add_argument("object",
 
 # }}
 
+# Add hash-object command argument {{
+
+argsp = argsubparsers.add_parser(
+    "hash-object",
+    help="Compute object ID and optionally creates\
+        a blob from a file")
+
+argsp.add_argument("-t",
+    metavar="type",
+    dest="type",
+    choices=["blob", "commit", "tag", "tree"],
+    default="blob",
+    help="Specify the type")
+
+argsp.add_argument("-w",
+    dest="write",
+    action="store_true",
+    help="Actually write the object into the database")
+
+argsp.add_argument("path",
+    help="Read object from <file>")
+
+# }}
+
 # }}}
 
 # Git Objects {{{
@@ -302,6 +326,20 @@ def object_write(obj, actually_write=True):
 
     return sha
 
+def object_hash(fd, fmt, repo=None):
+    data = fd.read()
+
+    # Choose constructor depending on
+    # object type found in header.
+    if   fmt==b'commit' : obj=GitCommit(repo, data)
+    elif fmt==b'tree'   : obj=GitTree(repo, data)
+    elif fmt==b'tag'    : obj=GitTag(repo, data)
+    elif fmt==b'blob'   : obj=GitBlob(repo, data)
+    else:
+        raise Exception("Unknown type %s!" % fmt)
+
+    return object_write(obj, repo)
+
 # }}
 
 # }}}
@@ -324,6 +362,20 @@ def cmd_cat_file(args):
 def cat_file(repo, obj, fmt=None):
     obj = object_read(repo, object_find(repo, obj, fmt=fmt))
     sys.stdout.buffer.write(obj.serialize())
+
+# }}
+
+# Hash-Object {{
+
+def cmd_hash_object(args):
+    if args.write:
+        repo = GitRepository(".")
+    else:
+        repo = None
+
+    with open(args.path, "rb") as fd:
+        sha = object_hash(fd, args.type.encode(), repo)
+        print(sha)
 
 # }}
 
