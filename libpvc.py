aeaca6ac1a1f99e8e7bf0797957d628b764da3e4
nihitb06 <nihitb06.dev@gmail.com>
2020-07-20 16:00:48 +0530

    Added Git Index Object

--- libpvc.py
+++ libpvc.py
@@ -303,6 +303,42 @@ class GitTree(GitObject):
 class GitTag(GitCommit):
     fmt = b'tag'
 
+
+# Git Index File Entry {{
+
+class GitIndexEntry(object):
+    ctime = None
+    """The last time a file's metadata changed.  This is a tuple (seconds, nanoseconds)"""
+
+    mtime = None
+    """The last time a file's data changed.  This is a tuple (seconds, nanoseconds)"""
+
+    dev = None
+    """The ID of device containing this file"""
+    ino = None
+    """The file's inode number"""
+    mode_type = None
+    """The object type, either b1000 (regular), b1010 (symlink), b1110 (gitlink). """
+    mode_perms = None
+    """The object permissions, an integer."""
+    uid = None
+    """User ID of owner"""
+    gid = None
+    """Group ID of ownner (according to stat 2.  Isn'th)"""
+    size = None
+    """Size of this object, in bytes"""
+    obj = None
+    """The object's hash as a hex string"""
+    flag_assume_valid = None
+    flag_extended = None
+    flag_stage = None
+    flag_name_length = None
+    """Length of the name if < 0xFFF (yes, three Fs), -1 otherwise"""
+
+    name = None
+
+# }}
+
 # }}}
 
 # Helper Functions {{{
